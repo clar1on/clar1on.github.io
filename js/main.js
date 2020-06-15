9ed999e9e2e8f9b8b197fd56da11456fd9d20bc8
Nick Hlushpenko <nick.hlushpenko@gmail.com>
2020-06-15 22:15:28 +0300

    Add main menu. Refactoring

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     $('#pagepiling').pagepiling({
+        menu: '#menu',
+        anchors: ['head', 'about', 'krasnarodyna', 'clarionio', 'contact'],
         navigation: {
-            tooltips: ['Головний слайд', 'Про мене', 'ФГ КраснаРодина', 'CLARIONIO', 'Контактна інформація']
-        }
+            tooltips: ['Головний слайд', 'Про мене', 'ФГ КраснаРодина', 'CLARIONIO', 'Контактна інформація'],
+        },
+        // loopTop: true,
+        // loopBottom: true
     });
 });
