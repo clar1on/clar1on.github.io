caaa066847230352d7f88354eea96d3e5fd3efa7
Nick Hlushpenko <nick.hlushpenko@gmail.com>
2020-06-16 14:55:19 +0300

    Add burger menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
     $('#pagepiling').pagepiling({
-        menu: '#menu',
-        anchors: ['head', 'about', 'krasnarodyna', 'clarionio', 'contact'],
         navigation: {
-            tooltips: ['Головний слайд', 'Про мене', 'ФГ КраснаРодина', 'CLARIONIO', 'Контактна інформація'],
-        },
-        // loopTop: true,
-        // loopBottom: true
+            tooltips: ['Головний слайд', 'Про мене', 'ФГ КраснаРодина', 'CLARIONIO', 'Контактна інформація']
+        }
     });
 });
