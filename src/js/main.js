c448439c064b8cac127f6c5307f10d1fdfa4f7ba
Nick Hlushpenko <nick.hlushpenko@gmail.com>
2022-04-23 16:08:00 +0300

    Update data and clean code

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,13 @@
 $(document).ready(function () {
     $('#pagepiling').pagepiling({
         menu: '#menu',
-        anchors: ['head', 'about', 'soft-skills', 'hard-skills', 'krasnarodyna', 'clarionio', 'berizka' , 'contact'],
+        anchors: ['head', 'soft-skills', 'hard-skills', 'krasnarodyna', 'clarionio', 'berizka', 'contact'],
         navigation: {
-            tooltips: ['Home', 'About me', 'Soft skills', 'Hard skils', 'KRASNARODYNA', 'CLARIONIO', 'BERIZKA', 'Contacts'],
+            tooltips: ['Home', 'Soft skills', 'Hard skils', 'KRASNARODYNA', 'CLARIONIO', 'BERIZKA', 'Contacts']
         }
     });
-
-
 });
 
 function closeMenuToggle() {
-   document.getElementById("menu__toggle").click();
+    document.getElementById("menu__toggle").click();
 }
-
-
